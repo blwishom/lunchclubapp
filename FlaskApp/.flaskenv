3153ee8c752d1020e86b0fcc85c9eea0806eb4bf
Blair Wishom <blwishom@gmail.com>
2022-12-19 09:57:04 -0800

    dependencies upgraded

--- FlaskApp/.flaskenv
+++ FlaskApp/.flaskenv
@@ -1,3 +1,1 @@
 FLASK_APP=app.py
-FLASK_ENV=development
-SECRET_KEY=4XJFfz5JK6fyqqEqm__A7Q   
--- FlaskApp/seeds/users.py
+++ FlaskApp/seeds/users.py
@@ -2,7 +2,11 @@ from models import db, User
 
 def seed_users():
     demo = User(
-        name='Demo User', username='demouser', email='demo@lunchclub.com', password='password', club_id=1)
+        name='Demo User', username='demouser', email='demo@lunchclub.com', password='password', club_id=1,
+        )
+    another_user = User(
+        name='Another User', username='another_user', email='another_user@lunchclub.com', password='password', club_id=2
+    )
 
     db.session.add(demo)
 
