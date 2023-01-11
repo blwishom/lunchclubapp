840384539b205361f305427f3ad0fdd1df6222d0
Blair Wishom <blwishom@gmail.com>
2023-01-11 10:43:55 -0800

    backend imports complete

--- FlaskApp/forms/login_form.py
+++ FlaskApp/forms/login_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
-from models import User
+from models import Member
 
 
 def user_exists(form, field):
