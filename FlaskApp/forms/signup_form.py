3255946dfb938363f35828776cd0ced0eee2026a
canjalal <vincent.shuali@gmail.com>
2023-02-18 18:10:54 -0800

    Install wtforms[email] package and start setting up member signup form, CSRF validation does fail

--- FlaskApp/forms/signup_form.py
+++ FlaskApp/forms/signup_form.py
@@ -1,7 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
-from models import Member
+from wtforms import StringField, PasswordField, IntegerField, SelectField
+from wtforms.validators import DataRequired, Email, ValidationError, Length
+from models import Member, Club
+
+"""
+    __tablename__ = 'members'
+    __table_args__ = (UniqueConstraint('username', name='uq_members_username'),)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    club_id = db.Column(db.Integer)
+    member_info = db.Column( member_type)
+    last_login = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    password_digest = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+"""
 
 
 def member_exists(form, field):
@@ -19,9 +36,18 @@ def username_exists(form, field):
     if member:
         raise ValidationError('Username is already in use.')
 
+def club_exists(form, field):
+    # Checking if club exists
+    if field.data is not None:
+        club_id = field.data
+        club = Club.query.filter(Club.id == club_id).first()
+        if club is None:
+            raise ValidationError('Club does not exist.')
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), member_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username', validators=[DataRequired(), username_exists, Length(min=3, max=20)])
+    email = StringField('email', validators=[DataRequired(), Email(), member_exists])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=6)])
+    club_id = IntegerField('club_id', validators=[club_exists])
+    member_info = SelectField('member_info', choices=[('regular', 'regular'), ('admin', 'admin'), ('banned', 'banned')], validators=[DataRequired()])
--- FlaskApp/services/member_services.py
+++ FlaskApp/services/member_services.py
@@ -1,19 +1,30 @@
 from models import Member, db
 from sqlalchemy.exc import SQLAlchemyError
 from flask import abort
+from forms import SignUpForm
+from wtforms import ValidationError
 
 def get_members():
     members = Member.query.all()
     return members
 
 def create_member(**member_data):
-    new_member = Member(**member_data)
     try:
-        db.session.add(new_member)
-        db.session.commit()
-        return new_member.to_dict()
-    except SQLAlchemyError as e:
-        return None
+        form = SignUpForm(**member_data)
+        valid = form.validate_on_submit()
+    except ValidationError as e:
+        return str(e)
+    
+    if valid:
+        new_member = Member(**member_data)
+        try:
+            db.session.add(new_member)
+            db.session.commit()
+            return new_member.to_dict()
+        except SQLAlchemyError as e:
+            return None
+    else:
+        return form.errors
 
 def get_member(id):
     member = Member.query.get_or_404(id)
