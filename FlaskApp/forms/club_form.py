13e5a3dae9168875f1fb3a07f4f062c348fbe309
canjalal <vincent.shuali@gmail.com>
2023-03-01 19:03:19 -0800

    Finish PATCH request for clubs

--- FlaskApp/forms/club_form.py
+++ FlaskApp/forms/club_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, Email, ValidationError, Regexp, Length
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError, Regexp, Length
 from models import Club
 '''
 id = db.Column(db.Integer, primary_key=True)
@@ -19,14 +19,13 @@ def club_exists(form, field):
         raise ValidationError(f"'{name}' is already the name of a club. Try another name")
 
 
-def joincode_matches(form, field):
-    # Checking if password matches
-    join_code = field.data
-    name = form.data['name']
-    club = Club.query.filter(Club.join_code == join_code).first()
-    if not club:
-        raise ValidationError('No such club exists.')
-
+# def joincode_matches(form, field):
+#     # Checking if password matches
+#     join_code = field.data
+#     name = form.data['name']
+#     club = Club.query.filter(Club.join_code == join_code).first()
+#     if not club:
+#         raise ValidationError('No such club exists.')
 
 class ClubForm(FlaskForm):
     name = StringField('Club Name:', validators=[DataRequired(), club_exists, Length(min=3, max=255, message="Name must be between 5 and 50 characters.")])
@@ -36,3 +35,21 @@ class ClubForm(FlaskForm):
     class Meta: # for testing purposes only. Need to reenable csrf for production
         csrf = False
 
+class EditClubForm(FlaskForm):
+    name = StringField('Club Name:', validators=[DataRequired(), Length(min=3, max=255, message="Name must be between 5 and 50 characters.")])
+    location = StringField('Location:', validators=[DataRequired(), Length(min=3, max=255, message="Location must be 3 characters or longer.")])
+    join_code = StringField(validators=[Regexp('^\d{6}$', message="Join code must be 6 digits of numbers.")])
+
+    # def set_data(self, club):
+    #     self.name.data = club.name
+    #     self.location.data = club.location
+    #     self.join_code.data = club.join_code
+    
+    def validate_name(self, field):
+        club = Club.query.filter_by(name=field.data).first()
+        print(f"self.id is {self.id}")
+        if club is not None and club.id != self.id:
+            raise ValidationError(f"'{field.data}' is already the name of a club. Try another name")
+
+    class Meta: # for testing purposes only. Need to reenable csrf for production
+        csrf = False
--- FlaskApp/services/club_services.py
+++ FlaskApp/services/club_services.py
@@ -2,7 +2,7 @@ from models import Club, db
 # from forms import ClubForm
 from sqlalchemy.exc import SQLAlchemyError
 from flask import abort, jsonify
-from forms import ClubForm
+from forms import ClubForm, EditClubForm
 from wtforms.validators import ValidationError
 
 def get_clubs():
@@ -35,11 +35,25 @@ def update_club(id, **club_data):
     club = Club.query.get_or_404(id)
     if (club is None):
         abort(404)
-    club.name = club_data.get('name', club.name)
-    club.location = club_data.get('location', club.location)
-    club.join_code = club_data.get('join_code', club.join_code)
     try:
-        db.session.commit()
-        return club.to_dict()
-    except SQLAlchemyError as e:
-        return None
+        form = EditClubForm(obj=club)
+        form.id = club.id
+    except ValidationError as e:
+        return str(e)
+
+    if form.validate_on_submit():
+        form.populate_obj(club)
+        try:
+            db.session.commit()
+            return club.to_dict()
+        except SQLAlchemyError as e:
+            return None
+    else:
+        return {'errors': str(form.errors)}, 400
+
+    
+
+    # club.name = club_data.get('name', club.name)
+    # club.location = club_data.get('location', club.location)
+    # club.join_code = club_data.get('join_code', club.join_code)
+    
